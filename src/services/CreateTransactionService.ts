0ab7864b283463c34d9361b7067d70430991e624
Rodrigo Gama <ro.gama91@gmail.com>
2020-05-02 14:49:47 -0300

    feat: implements CreateTransactionService

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,62 @@
-// import AppError from '../errors/AppError';
-
+import { getCustomRepository, getRepository } from 'typeorm';
+import AppError from '../errors/AppError';
 import Transaction from '../models/Transaction';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import Category from '../models/Category';
+
+interface RequestDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
+
+const ALLOWED_TYPES = ['income', 'outcome'];
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: RequestDTO): Promise<Transaction> {
+    if (!ALLOWED_TYPES.includes(type)) {
+      throw new AppError('Invalid transaction type.');
+    }
+
+    if (Number.isNaN(value) || value < 0) {
+      throw new AppError('Invalid transaction value.');
+    }
+
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+    const {
+      total: availableAmount,
+    } = await transactionsRepository.getBalance();
+
+    if (value > availableAmount && type === 'outcome') {
+      throw new AppError('Insufficient account balance.');
+    }
+
+    const categoriesRepository = getRepository(Category);
+    let transactionCategory = await categoriesRepository.findOne({
+      where: { title: category },
+    });
+
+    if (!transactionCategory) {
+      transactionCategory = categoriesRepository.create({ title: category });
+      await categoriesRepository.save(transactionCategory);
+    }
+
+    const transaction = await transactionsRepository.create({
+      title,
+      type,
+      value,
+      category: transactionCategory,
+    });
+
+    await transactionsRepository.save(transaction);
+
+    return transaction;
   }
 }
 
