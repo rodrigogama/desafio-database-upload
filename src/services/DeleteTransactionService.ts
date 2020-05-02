42092a62bf0740eacbaa034d5557ef015bf13a85
Rodrigo Gama <ro.gama91@gmail.com>
2020-05-02 14:50:05 -0300

    feat: implements DeleteTransactionService

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,22 @@
-// import AppError from '../errors/AppError';
+import { isUuid } from 'uuidv4';
+import { getCustomRepository } from 'typeorm';
+import AppError from '../errors/AppError';
+import TransactionsRepository from '../repositories/TransactionsRepository';
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute(id: string): Promise<void> {
+    if (!isUuid(id)) {
+      throw new AppError('Transaction not found.');
+    }
+
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+    const transaction = await transactionsRepository.findOne(id);
+
+    if (!transaction) {
+      throw new AppError('Transaction not found.');
+    }
+
+    await transactionsRepository.remove(transaction);
   }
 }
 
